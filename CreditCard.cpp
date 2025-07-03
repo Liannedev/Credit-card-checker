be2a0753faad38447f5c8c7ba5ae7d58b430ab7a
Jullianne <147238043+Liannedev@users.noreply.github.com>
2025-07-04 00:53:24 +0800

    Initial commit

---
+++ CreditCard.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int getDigit(int number);
+int addEvenNumbers(string cardNumber);
+int addOddNumbers(string cardNumber);
+ 
+
+int main(){
+  string cardNumber;
+  cout << "Enter your credit card number: ";
+  cin >> cardNumber;
+
+
+
+
+    return 0;
+} 
+
+int getDigit(int number){
+
+  return 0;
+}
+int addEvenNumbers(string cardNumber){
+  
+  return 0;
+}
+int addOddNumbers(string cardNumber){
+
+  return 0;
+}
